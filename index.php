64c8d48ce3d5e36b2f68e3869e89f60a2b4e6f27
rama <dwiramadhana5148@gmail.com>
2019-10-13 08:58:09 +0700

    menambahkan script html

--- index.php
+++ index.php
@@ -1,1 +1,10 @@
-index.php
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>Belajar Git - Project 01</title>
+    </head>
+    <body>
+        <p>Hello Indonesia</p>
+    </body>
+</html>
